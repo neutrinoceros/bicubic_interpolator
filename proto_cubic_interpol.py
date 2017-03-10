a6bb02173876b6b3a5beba65ee149c7ee2550012
Clément Robert <clement.robert@protonmail.com>
2017-03-10 09:55:14 +0100

    correctly interpolate on a narrow interval

--- proto_cubic_interpol.py
+++ proto_cubic_interpol.py
@@ -6,16 +6,16 @@ from numpy import random as rd
 import matplotlib.pyplot as plt
 
 #defintions
-def interpolation_Cubique(p0,p1,p2,p3,x) :
-    # dev note ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # I never took into account the x-coordinates of p0,p1,p2,p3
-    # this feels wrong on more than one level ...
-    # dev note ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A,B,C,D = 0.,0.,0.,0.
+def update_coefficients(x0,p0,x1,p1,x2,p2,x3,p3) :
+    # those formulas where calculated and copied/pasted from Mathematica
+    a=p0/((x0-x1)*(x0-x2)*(x0-x3))-p1/((x0-x1)*(x1-x2)*(x1-x3))+p2/((x0-x2)*(x1-x2)*(x2-x3))-p3/((x0-x3)*(-x1+x3)*(-x2+x3))
+    b=(p2*(x0+x1+x3))/((x0-x2)*(-x1+x2)*(x2-x3))+(p3*(x0+x1+x2))/((x0-x3)*(-x1+x3)*(-x2+x3))+(p1*(x0+x2+x3))/((x0-x1)*(x1-x2)*(x1-x3))-(p0*(x1+x2+x3))/((x0-x1)*(x0-x2)*(x0-x3))
+    c=-((p3*(x1*x2+x0*(x1+x2)))/((x0-x3)*(-x1+x3)*(-x2+x3)))-(p2*(x1*x3+x0*(x1+x3)))/((x0-x2)*(-x1+x2)*(x2-x3))-(p1*(x2*x3+x0*(x2+x3)))/((x0-x1)*(x1-x2)*(x1-x3))+(p0*(x2*x3+x1*(x2+x3)))/((x0-x1)*(x0-x2)*(x0-x3))
+    d=-((p0*x1*x2*x3)/((x0-x1)*(x0-x2)*(x0-x3)))+x0*((p1*x2*x3)/((x0-x1)*(x1-x2)*(x1-x3))+(p2*x1*x3)/((x0-x2)*(-x1+x2)*(x2-x3))+(p3*x1*x2)/((x0-x3)*(-x1+x3)*(-x2+x3)))
+    return a,b,c,d
 
-    a = -0.5*p0 + 1.5*p1 - 1.5*p2 + 0.5*p3
-    b =      p0 - 2.5*p1 + 2.0*p2 - 0.5*p3
-    c = -0.5*p0          + 0.5*p2
-    d = p1
+def interpolation_Cubique(a,b,c,d,p0,p1,p2,p3,x) :
     return d + c*x + b*x**2 + a*x**3
 
 # def interpolation_BiCubique(p00,p01,p02, ... ,x,y) :
@@ -26,24 +26,34 @@ def interpolation_Cubique(p0,p1,p2,p3,x) :
 #              f(p30,p31,p32,p33,y),x)
 
 #script
-N = 4
-x = np.linspace(-1,3,N+1)
+N = 10
+x = np.linspace(-1,2,N+1)
 data = rd.normal(1,1,N+1)
 
 nrad = 1000
-refined_x = np.linspace(-1,3,nrad+1)
+refined_x = np.linspace(-1,2,nrad+1)
 interpol_data = np.zeros(nrad+1)
+
 i_old = 0
+# uncomment this when ready to dev border conditions
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#X0,X1,X2,X3 = x   [i_old-2:i_old+2]
+#P0,P1,P2,P3 = data[i_old-2:i_old+2]
+#A,B,C,D     = update_coefficients(X0,P0,X1,P1,X2,P2,X3,P3)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 for i in range(nrad) :
+    update_required = False
     while (x[i_old] < refined_x[i]) :
         i_old += 1
-    if i_old > 1 and i_old < N : #forget about the borders for now
-        #print i, i_old
-        interpol_data[i] = interpolation_Cubique(data[i_old-2],
-                                                 data[i_old-1],
-                                                 data[i_old],
-                                                 data[i_old+1],
-                                                 refined_x[i])
+        update_required = True
+    if update_required and i_old > 2 and i_old < N-2:
+        X0,X1,X2,X3 = x   [i_old-2:i_old+2]
+        P0,P1,P2,P3 = data[i_old-2:i_old+2]
+        A,B,C,D = update_coefficients(X0,P0,X1,P1,X2,P2,X3,P3)
+        print refined_x [i], X0,X1,X2,X3
+    if i_old > 2 and i_old < N-2 : #forget about the borders for now
+        interpol_data[i] = interpolation_Cubique(A,B,C,D,P0,P1,P2,P3,refined_x[i])
 
 plt.scatter(x,data)
 #plt.scatter(refined_x,interpol_data, marker='+', color='r')
