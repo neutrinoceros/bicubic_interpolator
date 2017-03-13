8e52dabf2cc5a66d7172a78c397f32698e90ce43
Clément Robert <clement.robert@protonmail.com>
2017-03-13 09:55:47 +0100

    change how routines handle coefficient to make it closer to C

--- proto_cubic_interpol.py
+++ proto_cubic_interpol.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 # defintions -------------------------------------------------------------
 
-def update_coefficients(x0,p0,x1,p1,x2,p2,x3,p3) :
+def update_coefficients(x0,p0,x1,p1,x2,p2,x3,p3,a,b,c,d) :
     # those formulas where calculated and copied/pasted from Mathematica
     a=p0/((x0-x1)*(x0-x2)*(x0-x3))\
        -p1/((x0-x1)*(x1-x2)*(x1-x3))\
@@ -30,32 +30,30 @@ def update_coefficients(x0,p0,x1,p1,x2,p2,x3,p3) :
         +(p3*x1*x2)/((x0-x3)*(-x1+x3)*(-x2+x3)))
     return a,b,c,d
 
+
 def interpolation_Cubique(a,b,c,d,x) :
     return a*x**3 + b*x**2 + c*x + d
+
+# def main_loop(
 
 def interpolation_BiCubique(x00,y00,p00, x01,y01,p01, x02,y02,p02, x03,y03,p03,
                             x10,y10,p10, x11,y11,p11, x12,y12,p12, x13,y13,p13,
                             x20,y20,p20, x21,y21,p21, x22,y22,p22, x23,y23,p23,
                             x30,y30,p30, x31,y31,p31, x32,y32,p32, x33,y33,p33,
-                            x,y) :
-
+                            a0,b0,c0,d0,a1,b1,c1,d1,a2,b2,c2,d2,a3,b3,c3,d3,a,b,c,d,
+                            x,y):
     f = interpolation_Cubique#local alias
 
-    #dev note :
-    # it would be much more optimal to update coefficients outside of this function
-    A0,B0,C0,D0 = update_coefficients(x00,p00,x01,p01,x02,p02,x03,p03)
-    A1,B1,C1,D1 = update_coefficients(x10,p10,x11,p11,x12,p12,x13,p13)
-    A2,B2,C2,D2 = update_coefficients(x20,p20,x21,p21,x22,p22,x23,p23)
-    A3,B3,C3,D3 = update_coefficients(x30,p30,x31,p31,x32,p32,x33,p33)
-    P0 = f(A0,B0,C0,D0,x)
-    P1 = f(A1,B1,C1,D1,x)
-    P2 = f(A2,B2,C2,D2,x)
-    P3 = f(A3,B3,C3,D3,x)
-    A,B,C,D = update_coefficients(y00,P0,y10,P1,y20,P2,y30,P3)
+    p0 = f(a0,b0,c0,d0,x)
+    p1 = f(a1,b1,c1,d1,x)
+    p2 = f(a2,b2,c2,d2,x)
+    p3 = f(a3,b3,c3,d3,x)
+    #dev note : revise how exactly the functions should be entangled
+    a,b,c,d = update_coefficients(y00,p0,y10,p1,y20,p2,y30,p3)
 
     return f(A,B,C,D,y)
 
-# parameters -------------------------------------------------------------
+# #parameters -------------------------------------------------------------
 
 xwidth  = 10.
 xnb_pts = 6
@@ -83,6 +81,12 @@ ax = fig.gca(projection='3d')
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 
+A =B =C =D =0.
+A0=B0=C0=D0=0.
+A1=B1=C1=D1=0.
+A2=B2=C2=D2=0.
+A3=B3=C3=D3=0.
+
 for j in range(len(Y_new)) :
     interpol_data = np.zeros(data.shape)
     interpol_1d_x = np.zeros(len(X_new))
@@ -103,10 +107,17 @@ for j in range(len(Y_new)) :
             i_old += 1
             Xupdate_required = True
         if i_old > 1 and X_new[i] < X_old[xnb_pts-1] :
-            if Xupdate_required :
+            if Xupdate_required :                
                 X0,X1,X2,X3 = X_old [i_old-2:i_old+2]
                 P0,P1,P2,P3 = data  [j,i_old-2:i_old+2]
-                A,B,C,D = update_coefficients(X0,P0,X1,P1,X2,P2,X3,P3)
+                A,B,C,D = update_coefficients(X0,P0,X1,P1,X2,P2,X3,P3,A,B,C,D)
+
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~new {
+                # A0,B0,C0,D0 = update_coefficients(x00,p00,x01,p01,x02,p02,x03,p03,A0,B0,C0,D0)
+                # A1,B1,C1,D1 = update_coefficients(x10,p10,x11,p11,x12,p12,x13,p13,A1,B1,C1,D1)
+                # A2,B2,C2,C3 = update_coefficients(x20,p20,x21,p21,x22,p22,x23,p23,A2,B2,C2,D2)
+                # A3,B3,C3,D3 = update_coefficients(x30,p30,x31,p31,x32,p32,x33,p33,A3,B3,C3,D3)
+                #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ }
 
             interpol_1d_x[i] = interpolation_Cubique(A,B,C,D,X_new[i])
         else : #forget about the borders for now
